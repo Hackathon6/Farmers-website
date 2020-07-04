3f553fd45b7bc83b114225c78ec5aa51d4d8f853
jash6 <jashmakhija06@gmail.com>
2020-07-04 14:44:53 +0530

    backend updated

---
+++ myflaskapp/static/js/explore.js
@@ -0,0 +1,11 @@
+//Navbar starts
+var height =$('header').height();
+$(window).scroll(function(){
+    if($(this).scrollTop() > height){
+        $('.navbar').addClass('fixed');
+    }
+    else{
+        $('.navbar').removeClass('fixed');
+    }
+});
+// Navbar ends
---
+++ myflaskapp/static/js/home.js
@@ -0,0 +1,51 @@
+//Slideshow starts
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+//Slideshow ends
+
+$(document).ready(function () {
+  function showWindow() {
+    $("#main").show();
+    $("html body").css("overflow", "hidden");
+  }
+
+  function hideWindow() {
+    $("#main").hide();
+    $("html body").css("overflow", "scroll");
+  }
+
+  setTimeout(showWindow, 3000);
+  $("#close-btn").click(function () {
+    hideWindow();
+  });
+});
